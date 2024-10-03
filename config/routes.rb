b527cf8b62efbe3e4d6e823980a78fd882bf9345
rflores <ricardo.flores@penalolen.cl>
2024-10-03 20:53:36 -0300

    configuración gemas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   resources :tweets
+    member do
+      get "preview"
+    end
+  
+    collection do
+      get 'search'
+   end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,5 +19,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "tweets#index"
 end
